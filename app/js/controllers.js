54285586ec4ef871454fb0c141a6583b5e688bf2
Kate <katepdonahue@gmail.com>
2014-02-07 16:56:08 -0500

    Give dropdown a default

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,5 +7,7 @@ spacecatApp.controller('CatListCtrl', function($scope, $http) {
   $http.get("cats/cats.json").success(function(data) {
     $scope.cats = data;
   });
-  
+
+  $scope.orderProp = 'name';
+
 });
